093ad99e00c606b91fdcf784382199e1b1b9ca80
Alam <amenabeato@gmail.com>
2023-02-03 06:29:52 -0400

    updateds

--- Controllers/InvoiceController.cs
+++ Controllers/InvoiceController.cs
@@ -48,5 +48,67 @@ namespace API.Controllers
 
         }
 
+        private object GetPercentageByStatsus(IEnumerable<Invoice> src, IEnumerable<Invoice> dest)
+        {
+            var srcCount = src.Count() == 0 ? 1 : src.Count();
+            var destCount = dest.Count() == 0 ? 1 : dest.Count();
+            var m = (decimal)srcCount / destCount;
+            return new
+            {
+                destCount,
+                srcCount,
+                total = (decimal)srcCount / (decimal)destCount
+            };
+        }
+
+        [HttpPost("invoice/status")]
+        public async Task<IActionResult> GetInvoiceStatus()
+        {
+            var invoices = await _context.Invoices.ToListAsync();
+
+            var paidInvoices = invoices.Where(d => d.TotalPayed >= d.Total && d.Confirmed == true);
+            var unpaidInvoices = invoices.Where(d => d.TotalPayed <= d.Total && d.Confirmed == true);
+            var overdueInvoices = invoices.Where(d => d.CreditDates <= d.Date.Subtract(DateTime.Now).TotalDays);
+            var shouldBePayedInvoices = invoices.Where(d => d.CreditDates >= d.Date.Subtract(DateTime.Now).TotalDays);
+            var draftInvoices = invoices.Where(d => d.Confirmed == false);
+
+            var response = new
+            {
+                global = new
+                {
+                    count = invoices.Count,
+                    percentage = 100,
+                    total = invoices.Sum(d => d.Total)
+                },
+                paid = new
+                {
+                    count = paidInvoices.Count(),
+                    percentage = GetPercentageByStatsus(paidInvoices, invoices),
+                    total = paidInvoices.Sum(d => d.Total),
+                },
+                unpaid = new
+                {
+                    count = unpaidInvoices.Count(),
+                    percentage = GetPercentageByStatsus(unpaidInvoices, invoices),
+                    total = unpaidInvoices.Sum(d => d.Total)
+                },
+                overdue = new
+                {
+                    count = overdueInvoices.Count(),
+                    percentage = GetPercentageByStatsus(overdueInvoices, shouldBePayedInvoices),
+                    total = overdueInvoices.Sum(d => d.Total)
+                },
+                draft = new
+                {
+                    count = draftInvoices.Count(),
+                    percentage = GetPercentageByStatsus(invoices, draftInvoices),
+                    total = draftInvoices.Sum(d => d.Total)
+                }
+            };
+
+            return Ok(response);
+        }
+
+
     }
 }
